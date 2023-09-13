87e327390a7b7ba659adf51db185ea4af0f470ce
matheusgnreis <matheus@e-com.club>
2023-09-13 15:44:37 -0300

    chore: set url into buy and fix styles

---
+++ storefront.webpack.js
@@ -0,0 +1,10 @@
+const path = require('path')
+
+module.exports = () => ({
+  resolve: {
+    alias: {
+      './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/html/TheProduct.html'),
+      './js/TheProduct.js': path.resolve(__dirname, 'template/js/custom-js/js/TheProduct.js'),
+    }
+  }
+})
