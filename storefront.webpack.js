562f8d01a6b075dd14fb0faa15e4c93dde80fc96
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2023-10-04 14:47:44 -0300

    feat: create grid review

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -3,6 +3,7 @@ const path = require('path')
 module.exports = () => ({
   resolve: {
     alias: {
+      './base-config': path.resolve(__dirname, 'template/js/netlify-cms/base-config'),
       './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/html/TheProduct.html'),
       './js/TheProduct.js': path.resolve(__dirname, 'template/js/custom-js/js/TheProduct.js'),
     }
